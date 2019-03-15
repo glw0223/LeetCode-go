9ce122273d696c6c4ba3fe1b79946cc5e5bcb7e9
gaoliwen <>
2019-03-15 14:27:50 +0800

    comment

--- question31_40/question31_40.go
+++ question31_40/question31_40.go
@@ -1,5 +1,10 @@
 package question31_40
 
+/*
+思路：1、先确定nums[mid]和旋转点的关系，也就是nums[left]或者nums[right]的关系
+2、再判断单调的那个区间，使用常规二分法判断；else的逻辑就是移动另一边
+3、最后收敛条件一定是在单调区间上
+ */
 func A33_search(nums []int, target int) int {
 	length:=len(nums)
 	if length==0{
@@ -13,13 +18,13 @@ func A33_search(nums []int, target int) int {
 		}
 		if nums[left]<=nums[mid]{
 			if nums[left]<=target && target<=nums[mid]{
-				right=mid
+				right=mid-1
 			}else {
 				left=mid+1
 			}
 		}else {
 			if nums[mid]<=target && target<=nums[right] {
-				left=mid
+				left=mid+1
 			}else {
 				right=mid-1
 			}
